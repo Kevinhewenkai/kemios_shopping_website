4522402d1dd2287466becbc1d1e315f341cf4931
kevin <kevin@stepincto.com>
2024-06-24 21:06:03 +1000

    add filtering pagination and sorting backend api

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,8 +16,19 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts() {
-            return await _context.Products.ToListAsync();
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams) {
+            var query =  _context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query,
+                productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
 
         [HttpGet("{id}")]
@@ -24,6 +37,15 @@ namespace API.Controllers
             var product = await _context.Products.FindAsync(id);
             if (product == null) return NotFound();
             return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilter()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new {brands, types});
         }
     }
 }
---
+++ API/Extensions/ProductExtensions.cs
@@ -0,0 +1,46 @@
+using API.Entities;
+using Microsoft.VisualBasic;
+
+namespace API.Extensions
+{
+    public static class ProductExtensions
+    {
+        public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy) {
+            if (string.IsNullOrWhiteSpace(orderBy)) return query.OrderBy(p => p.Name);
+
+            query = orderBy switch {
+                "price" => query.OrderBy(p => p.Price),
+                "priceDesc" => query.OrderByDescending(p => p.Price),
+                _ => query.OrderBy(p => p.Name)
+            };
+
+            return query;
+        }
+
+        public static IQueryable<Product> Search( this IQueryable<Product> query, string searchTerm) {
+            if (string.IsNullOrEmpty(searchTerm)) return query;
+
+            var lowerCaseSearchTerm = searchTerm.ToLower();
+
+            return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
+        }
+
+        public static IQueryable<Product> Filter( this IQueryable<Product> query, string brands, string types) {
+            var brandList = new List<string>();
+            var typesList = new List<string>();
+
+            if (!string.IsNullOrEmpty(brands)) {
+                brandList.AddRange(brands.ToLower().Split(",").ToList());
+            }
+
+            if (!string.IsNullOrEmpty(types)) {
+                typesList.AddRange(brands.ToLower().Split(",").ToList());
+            }
+
+            query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Brand.ToLower()));
+            query = query.Where(p => typesList.Count == 0 || typesList.Contains(p.Type.ToLower()));
+
+            return query;
+        }
+    }
+}
